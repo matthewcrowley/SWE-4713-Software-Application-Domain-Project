44da1834b62042797628576e9cdf676fc19c2aa6
Matt Crowley <matthewcrowley2002@gmail.com>
2025-10-20 23:36:20 -0400

    Update regularaccountuser.jsx

--- sad-project/src/pages/regularaccountuser.jsx
+++ sad-project/src/pages/regularaccountuser.jsx
@@ -92,7 +92,7 @@ export default function Regularaccountuser({ setIsLoggedIn }) {
             <span className="welcome-text">Welcome,</span>
             <div>
               <div className="username">
-                {currentUser?.username || "martyjohnson12#"}
+                {currentUser?.username || "matthewwilliam#8"}
               </div>
               <span className="regularuser-badge">RegUser</span>
             </div>
