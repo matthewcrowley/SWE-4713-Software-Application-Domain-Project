3a1ae4c345f2d40cf715fc0b002f492e036cb8e1
KellerCoding <ktkellerbusiness@gmail.com>
2025-10-27 11:35:59 -0400

    Updated to Express Node Backend Which must interact with MongoDB
    Flask abandoned in favor of Node Express Backend. CRUD API to be developed within the branch

--- sad-project/src/TestPage.jsx
+++ sad-project/src/TestPage.jsx
@@ -1,72 +1,48 @@
-// Filename - TestPage.js
+// Filename - TestPage.jsx
 
 // Importing modules
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
-export default function TestPage() {
-    // usestate for setting a javascript
+function TestPage() {
+    // useState for setting a javascript
     // object for storing and using data
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [data, setdata] = useState({
-        name: "",
-        age: 0,
-        date: "YOU MUST",
-        programming: "CHANGE",
-    });
-    const num = 1;
+    const [array, setArray] = useState([]);
+
+    const fetchAPI = async () => {
+        const response = await axios.get("http://localhost:8080/api");
+        setArray(response.data.fruits);
+        setLoading(false);
+        console.log(response.data.fruits);
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("http://localhost:5000/data");
-                const data = await response.json();
-                setdata({name: data.name,
-                age: data.Age,
-                date: data.date,
-                programming: data.programming});
-            } catch (error) {
-                console.error("Error fetching user:", error);
-                setError("Failed to load users.");
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, []);
+        fetchAPI();
+    }, [loading]);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/data') // Hopefully this in our backend
-            .then( res => {
-                if (!res.ok) throw new Error('Network response was not ok');
-                return res.json();
-            })
-            .then(data => {
-                setdata({
-                    name: data.name,
-                    age: data.Age,
-                    date: data.Date,
-                    programming: data.programming,
-                });
-                setLoading(false)
-            })
-            .catch(err => console.error('Error loading accounts:', err));
-    }, [setdata,data]);
+
+
 
 
 
     return (
         <div className="App">
             <header className="App-header">
+                {loading && <p>Loading...</p>}
+                {error && <p>Error: {error}</p>}
                 <h1>React and Flask</h1>
-                {/* Calling a data from setdata for showing */}
-                <p>{data.name}</p>
-                <p>{data.age}</p>
-                <p>{data.date}</p>
-                <p>{data.programming}</p>
-
+                {
+                    array.map((fruit, index) => (
+                        <div key={index}>
+                      <p>{fruit}</p>
+                        <br/>
+                        </div>
+                    ))}
             </header>
         </div>
     );
 }
+export default TestPage;
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+    res.json({"fruits": ["apples", "orange", "banana"]});
+
+    });
+
+app.listen(8080, () => console.log("Server is running on port 8080"));
+
