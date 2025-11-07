7a73cbe7112419ea735e6481d1427b1b9e750439
Matt Crowley <matthewcrowley2002@gmail.com>
2025-11-06 19:51:37 -0500

    Fix Account Ledger Account Name and Number Text Bug

--- sad-project/src/pages/AccountLedger.jsx
+++ sad-project/src/pages/AccountLedger.jsx
@@ -180,7 +180,7 @@ const AccountLedger = () => {
                     accountName = entryWithName?.accountName || 'Ledger';
                   }
 
-                  return `${accountNumber} - ${accountName}`;
+                  return <span style={{color: 'black'}}>{`${accountNumber} - ${accountName}`}</span>;
                 })()}
               </h2>
 
