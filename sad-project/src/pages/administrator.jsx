d183c094575883ac2f7b4c260aac66d96fcc04af
Kendal Elison <kelison022@gmail.com>
2025-11-13 13:07:11 -0500

    Updated navigation bar

--- sad-project/src/pages/administrator.jsx
+++ sad-project/src/pages/administrator.jsx
@@ -43,23 +43,6 @@ export default function Administrator({ setIsLoggedIn }) {
       icon: "📈",
       path: "/eventlog",
     },
-    {
-      title: "Reports",
-      description: "Generate financial reports",
-      icon: "📊",
-      path: "/reports",
-    },
-    {
-      title: "Search",
-      description: "Find accounts and transactions",
-      icon: "🔍",
-    },
-    {
-      title: "Ledger",
-      description: "Show ledger of Accounts",
-      icon: "📙",
-      path: "/Ledger"
-    },
 
   ];
 
