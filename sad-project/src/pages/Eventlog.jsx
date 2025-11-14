adb804bf10197169a7b0eb00bf6387ea44c8ec2b
Matt Crowley <matthewcrowley2002@gmail.com>
2025-11-13 20:28:10 -0500

    Revert "BEWARE API CALLS CHANGE"
    This reverts commit 1fa2444719713a589b2700ac90e2c090e16cc935.

--- sad-project/src/pages/Eventlog.jsx
+++ sad-project/src/pages/Eventlog.jsx
@@ -22,7 +22,7 @@ const Eventlog = () => {
   useEffect(() => {
     const fetchCurrentUser = async () => {
       try {
-        const response = await fetch("${import.meta.env.VITE_API_URL}/api/curUser");
+        const response = await fetch("http://localhost:3000/api/curUser");
         const data = await response.json();
         setCurrentUser(data.currentUser || null);
       } catch (err) {
@@ -34,7 +34,7 @@ const Eventlog = () => {
 
   // Fetch event logs
   useEffect(() => {
-  fetch('${import.meta.env.VITE_API_URL}/api/eventlog')
+  fetch('http://localhost:3000/api/eventlog')
     .then((res) => res.json())
     .then((data) => {
       console.log("Eventlog API response:", data); // <- Check this
