8b0cf88b8e77c166cfc107148b073292c347c107
Perrydj306 <perrydj306@gmail.com>
2025-12-03 13:26:34 -0500

    Got rid of the default profile image

--- sad-project/src/pages/AccountManagement.jsx
+++ sad-project/src/pages/AccountManagement.jsx
@@ -22,7 +22,6 @@ import {
   TableRow,
   Chip,
 } from "@mui/material";
-import defaultProfile from "../assets/defaultprofile.png";
 import "./AccountManagement.css";
 import { useNavigate, Link } from "react-router-dom";
 import HelpButton from "../components/HelpButton";
